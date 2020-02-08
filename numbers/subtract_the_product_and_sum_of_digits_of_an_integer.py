55b7dfcb422b94d5ddc0d690289afb2afa3ee8d8
Abhijit Mohapatra <abhijit.mohapatra@lowes.com>
2020-02-08 20:02:24 +0530

    Adding code to find the difference between the product of its digits and the sum of its digits.

---
+++ numbers/subtract_the_product_and_sum_of_digits_of_an_integer.py
@@ -0,0 +1,48 @@
+'''
+Given an integer number n,
+return the difference between the product of its digits and the sum of its digits.
+
+
+Example 1:
+
+Input: n = 234
+Output: 15
+Explanation:
+Product of digits = 2 * 3 * 4 = 24
+Sum of digits = 2 + 3 + 4 = 9
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation:
+Product of digits = 4 * 4 * 2 * 1 = 32
+Sum of digits = 4 + 4 + 2 + 1 = 11
+Result = 32 - 11 = 21
+
+
+Constraints:
+
+1 <= n <= 10^5
+
+'''
+
+#from typing import int
+
+class Solution():
+    def subtractProductAndSum(self, n: int) ->int:
+        if (n>=1 and n<=100000):
+            prd=1
+            sum=0
+            for ele in str(n):
+                prd=prd*int(ele)
+                sum=sum+int(ele)
+            return prd-sum
+
+def main():
+    sol =Solution()
+    print(sol.subtractProductAndSum(4421))
+    print(sol.subtractProductAndSum(234))
+
+if __name__ == '__main__':
+    main()
